69fbde338899b64ff87d25349ebaf9e63d28c14b
Tu Nguyen <im2nguyen@users.noreply.github.com>
2020-10-21 12:21:01 -0700

    Simplify configuration

--- web.tf
+++ web.tf
@@ -4,7 +4,6 @@ resource "aws_instance" "web" {
   ami                    = "ami-a0cfeed8"
   instance_type          = "t2.micro"
   user_data              = file("init-script.sh")
-  vpc_security_group_ids = [aws_security_group.web-sg.id]
 
   tags = {
     Name = random_pet.sg.id
