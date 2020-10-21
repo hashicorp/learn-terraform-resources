16d772ad3555f938d1922c590d8a9ff706f6af7e
Tu Nguyen <im2nguyen@users.noreply.github.com>
2020-10-21 10:29:49 -0700

    simplify configuration, remove elb

--- web.tf
+++ web.tf
@@ -29,32 +29,6 @@ resource "aws_security_group" "web-sg" {
   }
 }
 
-resource "aws_elb" "webapp" {
-  name               = "${random_pet.sg.id}-elb"
-  availability_zones = ["us-west-2a", "us-west-2b", "us-west-2c"]
-
-  listener {
-    instance_port     = 80
-    instance_protocol = "http"
-    lb_port           = 80
-    lb_protocol       = "http"
-  }
-
-  health_check {
-    healthy_threshold   = 2
-    unhealthy_threshold = 2
-    timeout             = 3
-    target              = "HTTP:80/"
-    interval            = 30
-  }
-
-  instances = aws_instance.web.*.id
-}
-
 output "web-address" {
-  value = aws_instance.web.*.public_dns
-}
-
-output "elb-address" {
-  value = aws_elb.webapp.dns_name
+  value = aws_instance.web.public_dns
 }
