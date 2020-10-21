c29c6568e852f476420a926c079c847ab2d709aa
Tu Nguyen <im2nguyen@users.noreply.github.com>
2020-10-21 10:26:05 -0700

    simplify init configuration

--- web.tf
+++ web.tf
@@ -11,23 +11,6 @@ resource "aws_instance" "web" {
   }
 }
 
-resource "aws_security_group" "web-sg" {
-  name = "${random_pet.sg.id}-sg"
-  ingress {
-    from_port   = 80
-    to_port     = 80
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-}
-
 output "web-address" {
   value = aws_instance.web.public_dns
 }
