d7bf59140ea4b3397cb6850580e043535689ef1b
Tu Nguyen <im2nguyen@users.noreply.github.com>
2020-10-29 14:43:02 -0700

    update output values

--- web.tf
+++ web.tf
@@ -1,15 +1,19 @@
 resource "random_pet" "name" {}
 
 resource "aws_instance" "web" {
-  ami                    = "ami-a0cfeed8"
-  instance_type          = "t2.micro"
-  user_data              = file("init-script.sh")
+  ami           = "ami-a0cfeed8"
+  instance_type = "t2.micro"
+  user_data     = file("init-script.sh")
 
   tags = {
     Name = random_pet.name.id
   }
 }
 
-output "web-address" {
+output "domain-name" {
   value = aws_instance.web.public_dns
+}
+
+output "application-url" {
+  value = "${aws_instance.web.public_dns}/index.php"
 }
