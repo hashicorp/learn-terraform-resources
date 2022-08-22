b8d121db9b9101fcc1ebb29aac6c05d9029772bc
ritsok <msokolova13@gmail.com>
2022-08-22 17:00:53 +0200

    Upgrade providers, add cloud block

--- terraform.tf
+++ terraform.tf
@@ -1,10 +1,23 @@
 terraform {
+
+  /* Uncomment this block to use Terraform Cloud for this tutorial
+	cloud {
+			organization = "organization-name"
+			workspaces {
+				name = "learn-terraform-*"
+			}
+	}
+	*/
+
   required_providers {
     aws = {
-      source = "hashicorp/aws"
+      source  = "hashicorp/aws"
+      version = "~> 4.15.0"
     }
     random = {
       source = "hashicorp/random"
     }
   }
+
+  required_version = "~> 1.2.0"
 }
