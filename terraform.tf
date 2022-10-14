23a2821e596ca71618d4943d417dc6172ce6fd9f
ritsok <msokolova13@gmail.com>
2022-10-14 12:01:37 -0400

    whitespace fix

--- terraform.tf
+++ terraform.tf
@@ -1,13 +1,13 @@
 terraform {
 
   /* Uncomment this block to use Terraform Cloud for this tutorial
-	cloud {
-			organization = "organization-name"
-			workspaces {
-				name = "learn-terraform-*"
-			}
-	}
-	*/
+  cloud {
+      organization = "organization-name"
+      workspaces {
+        name = "learn-terraform-*"
+      }
+  }
+  */
 
   required_providers {
     aws = {
