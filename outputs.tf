328d1d79f84e3078c259808f42fa94f3b90eb3d3
aymon <mohamed_ayman_07@hotmail.com>
2023-01-03 20:44:52 +0200

    Updating the init-script so that it would fit the current AMI
    modified the init-script so that it should suits AMI Linux 2 as AMI
    Linux has no more support as announced by AWS here: https://aws.amazon.com/amazon-linux-ami/
    Also made some additions like adding input variable for the region with
    default value as well as adding another output variable for the IP
    address of the ec2 instance

--- outputs.tf
+++ outputs.tf
@@ -5,3 +5,7 @@ output "domain-name" {
 output "application-url" {
   value = "${aws_instance.web.public_dns}/index.php"
 }
+
+output "application-ip-address" {
+  value = "${aws_instance.web.public_ip}"
+}
--- terraform.tf
+++ terraform.tf
@@ -19,5 +19,5 @@ terraform {
     }
   }
 
-  required_version = "~> 1.2.0"
+  required_version = ">= 1.2.0"
 }
